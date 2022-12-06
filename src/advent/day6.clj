d5a4d446a536d11a42a0b9035c97fa389468f43d
jjcohen-rvu <James.Cohen@rvu.co.uk>
2022-12-06 10:19:48 +0000

    Tidy up

--- src/advent/day6.clj
+++ src/advent/day6.clj
@@ -1,20 +1,11 @@
 (ns advent.day6
   (:require [clojure.string :as string]
             [clojure.set :refer [subset? intersection]]))
-
-;; (defn parse-int [x] (Integer/parseInt x))
-
-(def text (->> (slurp "/Users/james/learn/advent/resources/day6.txt")))
-
-;; (def text "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
-
 (def offset 14)
-
 (def search
-  (loop [t text
+  (loop [t (->> (slurp "/Users/james/learn/advent/resources/day6.txt"))
          acc offset]
     (if
      (= (count (distinct (take offset t))) offset)
       acc
       (recur (rest t) (inc acc)))))
-
