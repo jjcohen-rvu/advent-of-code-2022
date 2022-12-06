e435b385bfb8f5143191f5e14880676a9abe1a64
jjcohen-rvu <James.Cohen@rvu.co.uk>
2022-12-06 10:20:53 +0000

    Add andwers

--- src/advent/day6.clj
+++ src/advent/day6.clj
@@ -1,11 +1,13 @@
 (ns advent.day6
   (:require [clojure.string :as string]
             [clojure.set :refer [subset? intersection]]))
-(def offset 14)
-(def search
+(defn search
+  [n]
   (loop [t (->> (slurp "/Users/james/learn/advent/resources/day6.txt"))
          acc offset]
     (if
      (= (count (distinct (take offset t))) offset)
       acc
       (recur (rest t) (inc acc)))))
+(prn (search 4))
+(prn (search 14))
